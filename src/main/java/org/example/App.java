5c7a9c5e7705aa537225db4d104d17bbd52ba2c0
AndreiHirlav <148218258+oofracer@users.noreply.github.com>
2024-05-11 12:09:32 +0300

    final

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ import org.example.presentation.View;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args )
     {
         View vw = new View();
 
